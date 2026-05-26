607a318ed0353cdffc7d1813946fa991c46e7c93
Ajinkya Saivar <172878214+ajinkya682@users.noreply.github.com>
2026-05-26 13:06:22 +0530

    Implement authentication controllers for user registration and login

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,7 +1,13 @@
 const express = require("express");
+
+const {
+  registerController,
+  loginController,
+} = require("../controllers/auth.controller");
+
 const router = express.Router();
 
-router.post("/register");
-router.post("/login");
+router.post("/register", registerController);
+(router.post("/login"), loginController);
 
 module.exports = router;
